5a20056850ac665b51256da907e242172df99e14
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-12 14:43:13 +0700

    Add files via upload

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,4 +1,4 @@
-<br><?php include "../config/copyright.php";?><br><br><!-- /#page-wrapper
+<br><?php include "../config/copyright.php";?><br><br><!-- /#page-wrapper -->
 <!-- JavaScript -->
 <script type="text/javascript" src="../vendors/js/font-awesome.js"></script>
 <script type="text/javascript" src="../vendors/js/jquery-1.10.2.js"></script>
